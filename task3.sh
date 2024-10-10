dbaae79696756947b4ec304baed0aa7e97732bc3
Prithish Samanta <prithishsamanta@gmail.com>
2024-10-10 00:42:57 -0400

    Update task3.sh

--- task3.sh
+++ task3.sh
@@ -1,1 +1,1 @@
-awk -F',' '$3 == 2 && $13 == "S"' titanic.csv | sed 's/female/F/: s/male/M' | awk -F',' '$7 != "" {sum += $7; count++} END {print sum/count}'
+awk -F',' '$3 == 2 && $13 == "S"' titanic.csv | sed 's/female/F/; s/male/M/'  | awk -F',' '$7 != "" {sum += $7; count++} END {if (count > 0) $avg_age = sum/count; print "Average Age: " $avg_age}'
