4edd74273275db51e9c9b01d9e4eed26b07ed75e
Sweekar Burji <sweekarvb@gmail.com>
2024-10-30 12:02:53 -0400

    Update task3.sh to write output to new file

--- task3.sh
+++ task3.sh
@@ -1,2 +1,15 @@
-awk -F',' '$3 == 2 && $13 == "S"' titanic.csv | sed 's/female/F/; s/male/M/'
-awk -F',' '$3 == 2 && $13 == "S"' titanic.csv | sed 's/female/F/; s/male/M/'  | awk -F',' '$7 != "" {sum += $7; count++} END {if (count > 0) $avg_age = sum/count; print "Average Age: " $avg_age}'
+#!/bin/bash
+
+awk -F ',' '
+BEGIN {OFS=","}
+NR == 1 {print $0; next}
+$3 == 2 && $13 ~ /S/ {
+    gsub(/female/, "F", $0)
+    gsub(/male/, "M", $0)
+    print $0
+    if ($7 != "") {sum += $7; count++}
+}
+END {
+    if (count > 0) print "Average Age:" sum / count
+    else print "No valid ages found."
+}' titanic.csv | tee task3_filtered_output.csv
